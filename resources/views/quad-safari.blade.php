28245fb2d5a700895a2ff6b68a1f35d1b43bca42
AleksandarPHP <acocoaj123@gmail.com>
2024-12-07 15:58:28 +0100

    quad safari page

--- resources/views/quad-safari.blade.php
+++ resources/views/quad-safari.blade.php
@@ -1,32 +1,36 @@
-@include('partials/header')
-
+@extends('layouts.app')
+    @section('title', Helper::title(124))
+    @section('description', Helper::description(124))
+    @section('content')
 <main>
     <section class="career">
+        @php $text =  Helper::text(125) @endphp
         <div class="bg center" style="background-image: url('{{asset("assets/images/quad-bike-hero.jpg")}}');"></div>
         <div class="container">
             <div class="content-wrapper">
-                <h1 data-aos="fade-right" data-aos-duration="1500" data-aos-delay="250">Quad Safari</h1>
-                <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="750">
-                    Olimijska planina Jahorina je veoma poznata ski destinacija. Međutim, ono što tokom ljeta možete doživjeti na Jahorini sigurno vas neće ostaviti ravnodušnim.
-                    Jahorina obiluje sa veoma raznovrsnom florom, gdje možete vidjeti dosta ljekovitih trava, borovnice, brusnice, gljive… Prelijepe šume borova, bijelog javora – po kome je i dobila ime, bistre potoke i zadivljujuće planinske masive, koji se pružaju u nedogled.
-                </p>
+                @isset($text->title)
+                <h1 data-aos="fade-right" data-aos-duration="1500" data-aos-delay="250">{{$text->title}}</h1>
+                @endisset
             </div>
         </div>
     </section>
     @include('partials/booking')
     <section class="villa-termag">
-        <div class="bg center" style="background-image: url('{{asset("assets/images/quad-bg.jpg")}}');"></div>
+        @php $text =  Helper::text(126) @endphp
+        <div class="bg center" style="background-image: url('{{asset("storage/".$text->image)}}');"></div>
         <div class="container">
             <div>
                 <div class="cardd" data-aos="fade-right" data-aos-duration="600">
-                    <h2>Quad avantura po vašoj mjeri</h2>
+                    @isset($text->title)
+                    <h2>{{$text->title}}</h2>
+                    @endisset
+                    
+                    @isset($text->text)
                     <p class="txt">
-                        Ako želite na lakši, udobniji i jednostavniji način da doživite Jahorinu tokom ljeta onda je quad safari pravi izbor za vas. Termag hotel posjeduje najnovija CanAm vozila koja garantuju udobnu i sigurnu vožnju, a naši profesionalni vodiči će vam pokazati i najskrivenije kutke Jahorine.
-                        Za sve one koji se odluče na ovaj adrenalinski sadržaj hotela Termag obezbjedii smo i kompletnu zaštitnu opremu: kacige, naočale, rukavice, windstop jakne, pa čak i kabanice u slučaju kiše.
-                        Vožnja quad vozila na Jahorini je nezaboravno iskustvo koje ne smijete propustiti. Naše osoblje je tu da quad safari prilagodi baš vama, bilo da je pojedinačna vožnja, grupna vožnja ili porodična, doživljaj je neponovljiv. Tokom quad safarija možete obići najvisočije vrhove Jahorine i uživati u pogledu koji oduzima dah, jer se sa vrha vide sve planine u okruženju. Ili pak možete uživati u uzbudljivoj vožnji po šumskim stazama nakon kojih vas čeka planinska užina u prirodi, na prelijepom vidikovcu.
-                        Dovoljno je da se obratite našem ljubaznom osoblju na recepciji i rezervišete vaš termin za najuzbudljiviju vožnju ovog ljeta.
+                        {!!$text->text!!}
                     </p>
-                    <a href="#" class="btnn btn_primary">Book now</a>
+                    @endisset
+                    @if($text->urlTitle!='' && $text->url)<a href="{{Helper::url($text->url)}}" class="btnn btn_primary">{{$text->urlTitle}}</a>@endif
                 </div>
             </div>
         </div>
@@ -43,4 +47,4 @@
     </section>
 </main>
 
-@include('partials/footer')
+@endsection
