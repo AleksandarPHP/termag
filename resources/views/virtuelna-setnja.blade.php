698f38a82cfce62a174b3a78285270490ac898bd
AleksandarPHP <acocoaj123@gmail.com>
2024-12-07 14:43:45 +0100

    Add virtual a walk

--- resources/views/virtuelna-setnja.blade.php
+++ resources/views/virtuelna-setnja.blade.php
@@ -1,41 +1,60 @@
-@include('partials/header')
-
+@extends('layouts.app')
+    @section('title', Helper::title(193))
+    @section('description', Helper::description(193))
+    @section('content')
 <main>
     <section class="weather gallery-intro">
+        @php $text =  Helper::text(194) @endphp
         <div class="bg" style="background-image: url('{{asset("assets/images/main-bg.jpg")}}');"></div>
         <div class="container">
             <div class="wrapper">
-                <h1 class="title-larger">Virtuelna Šetnja</h1>
-                <p class="txt">
-                    Želimo vam dobrodošlicu u Virtuelno istraživanje Jahorine. Uz pomoć tehnologije, bićete u mogućnosti da, na potpuno realan način, virtuelno istražite prirodne ljepote ovog magičnog područja. Zaljubite se u nestvarnost prirode Jahorine u toplini svoga doma prije nego što nam dođete u posjetu.
-                </p>
+                @isset($text->title)
+                <h1 class="title-larger">{{$text->title}}</h1>
+                @endisset
+                @isset($text->text)
+                    {!!$text->text!!}
+                @endisset
             </div>
         </div>
     </section>
 
     <section class="wellness-spa">
-        <div class="bg" style="background-image: url('{{asset("assets/images/3d-bg.jpg")}}');"></div>
+        @php $text =  Helper::text(195) @endphp
+        <div class="bg" style="background-image: url('{{asset("storage/".$text->image)}}');"></div>
         <div class="container">
             <div class="cardd" data-aos="fade-left" data-aos-duration="600">
-                <h2>3D tehnologija</h2>
+                @isset($text->title)
+                <h2>{{$text->title}}</h2>
+                @endisset
+                
+                @isset($text->text)
                 <p class="txt">
-                    Kako biste na potpuno realan način doživjeli impozantnu prirodu Jahorine, koristimo 3D tehnologiju i pružamo vam mogućnost da istražite svaki kutak. Jasno i precizno ćete doživjeti svaki segment ove planinske ljepotice.</p>
-                <a href="#" class="btnn btn_primary">Book now</a>
+                    {!!$text->text!!}
+                </p>
+                @endisset
+                @if($text->urlTitle!='' && $text->url)<a href="{{Helper::url($text->url)}}" class="btnn btn_primary">{{$text->urlTitle}}</a>@endif
             </div>
         </div>
     </section>
 
     <section class="weather gallery-intro interactive-walk">
+        @php $text =  Helper::text(196) @endphp
         <div class="container">
             <div class="wrapper">
-                <h2 class="title-smaller">Interaktivna šetnja</h2>
+                @isset($text->title)
+                <h2 class="title-smaller">{{$text->title}}</h2>
+                @endisset
+                @isset($text->text)
                 <p class="txt">
-                    Uz posebnu tehnologiju, omogućili smo vam da istražite pejzaže, staze i impresivne planinske vrhove prije vašeg dolaska na Jahorinu. Uz specifične kontrole imaćete utisak da stvarno šetate nestvarnim predjelima ovog planinskog raja. Doživite nestvarnost predijela Jahorine iz svog doma i uvjerite se zašto nas morate posjetiti.
+                    {!!$text->text!!}
                 </p>
-                <p>api</p>
+                @endisset
+            </div>
+            <div class="wrapper">
+                <iframe src="https://termag.soft4tech.com/VR/ALL/zima/bh/index.html" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="500px" width="100%" allowfullscreen></iframe>
             </div>
         </div>
     </section>
 </main>
 
-@include('partials/footer')
+@endsection
