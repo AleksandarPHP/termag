8514c2ca1af01d9f807212ce568f773387d13508
AleksandarPHP <acocoaj123@gmail.com>
2025-03-12 20:19:06 +0100

    fix: pages

--- resources/views/ski-skola.blade.php
+++ resources/views/ski-skola.blade.php
@@ -13,9 +13,9 @@
                     @isset($text->title)
                     <h1 data-aos="fade-right" data-aos-duration="1500" data-aos-delay="250">{{$text->title}}</h1>
                     @endisset
-                    @isset($text->subtitle)
-                    <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="500">
-                        {{$text->subtitle}}
+                    @isset($text->text)
+                    <p class="txt">
+                        {!!$text->text!!}
                     </p>
                     @endisset
                     @if($text->urlTitle!='' && $text->url)<a href="{{Helper::url($text->url)}}" class="btnn btn_primary">{{$text->urlTitle}}</a>@endif
@@ -25,16 +25,16 @@
     </section>
 
     <section class="wellness-spa">
-        @php $text =  Helper::text(149) @endphp
+        @php $text =  Helper::text(150) @endphp
         <div class="bg" style="background-image: url('{{asset("storage/".$text->image)}}');"></div>
         <div class="container">
             <div class="cardd" data-aos="fade-left" data-aos-duration="600">
                 @isset($text->title)
                 <h1 data-aos="fade-right" data-aos-duration="1500" data-aos-delay="250">{{$text->title}}</h1>
                 @endisset
-                @isset($text->subtitle)
-                <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="500">
-                    {{$text->subtitle}}
+                @isset($text->text)
+                <p class="txt">
+                    {!!$text->text!!}
                 </p>
                 @endisset
                 @if($text->urlTitle!='' && $text->url)<a href="{{Helper::url($text->url)}}" class="btnn btn_primary">{{$text->urlTitle}}</a>@endif
