047d89d4567d180257ff89e7a7d3bc8ffa074e86
AleksandarPHP <acocoaj123@gmail.com>
2025-03-22 23:04:11 +0100

    feat: new package

--- resources/views/challenge-race.blade.php
+++ resources/views/challenge-race.blade.php
@@ -70,6 +70,61 @@
             </div>
         </div>
     </section>
+    <section class="special registration packages  registration-2">
+        <div class="bg" style="opacity: 0.2; z-index: -1; background-image: url('{{asset("assets/images/main-bg.webp")}}');"></div>
+        <img src="{{asset('assets/images/castle.png')}}" alt="zamak termag">
+        <div class="container">
+            @php
+            $packages = App\Models\Package::where('special_view', 2)->where('is_active', 1)->get();
+        @endphp
+        @foreach ($packages as $package)
+        <div class="text-center">
+            <h3>{{__('Register')}}:</h3>
+            <h2 class="title-smaller">{{$package->title}}</h2>
+            <br>
+        <nav class="pills-wrapper">
+            <div class="nav nav-tabs" id="nav-tab2" role="tablist">
+            @foreach ($package->options as $item)
+            <button class="nav-link @if ($loop->first)active @endif " id="nav-home-tab{{$item->id}}" data-bs-toggle="tab" data-bs-target="#nav-home{{$item->id}}" type="button" role="tab" aria-controls="nav-home{{$item->id}}" aria-selected="true">{{$item->title}}</button>
+            @endforeach
+            </div>
+        </nav>
+        </div>
+
+        <div class="tab-content" id="nav-tabContent">
+            @foreach ($package->options as $item)
+            <div class="tab-pane fade show @if ($loop->first) active @endif" id="nav-home{{$item->id}}" role="tabpanel" aria-labelledby="nav-home-tab{{$item->id}}" tabindex="0">
+                {!!$item->description!!}
+                <br>
+                <h3>{{$item->title}}</h3>
+                <div class="row row-2">
+                    @for($i = 0; $i < count($item->options[0]); $i++)
+                    <div class="col-md-6">
+                        @isset($item->options[0][$i])
+                        <div>
+                            <img src="{{asset('storage/'.$item->getTranslation('options', 'sr', false)[1][$i])}}" alt="ikonica">
+                            <p>
+                                {{$item->options[0][$i]}}
+                            </p>
+                        </div>
+                        @endisset
+                    </div>
+                    @endfor
+                </div>
+                <br>
+                <div class="text-center mt-5">
+                    {!!$package->text!!}
+                    <br>
+                    <a href="{{$package->url}}" class="btnn btn_primary mt-4">{{$package->urlTitle}}</a>
+                </div>
+            </div>  
+            <br>
+            @endforeach
+        </div>
+        @endforeach
+        </div>
+        </div>
+    </section>
     @include('partials/socials')
 </main>
 
