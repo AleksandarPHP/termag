dd32d08f8ed3c7a8ed95afdbd7cc088aa2e1f008
AleksandarPHP <acocoaj123@gmail.com>
2025-03-24 16:30:05 +0100

    feat: new section

--- resources/views/svadbe.blade.php
+++ resources/views/svadbe.blade.php
@@ -59,6 +59,42 @@
             </div>
         </div>
     </section>
+    <section class="villa-termag">
+        @php $text =  Helper::text(251) @endphp
+        <div class="bg" style="background-image: url('{{asset("storage/".$text->image)}}');"></div>
+        <div class="container">
+            <div>
+                <div class="cardd" data-aos="fade-right" data-aos-duration="600">
+                    @isset($text->title)
+                    <h2>{{$text->title}}</h2>
+                    @endisset
+                    
+                    @isset($text->text)
+                    <p class="txt">
+                        {!!$text->text!!}
+                    </p>
+                    @endisset
+                    @if($text->urlTitle!='' && $text->url)<a href="{{Helper::url($text->url)}}" class="btnn btn_primary">{{$text->urlTitle}}</a>@endif
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="race">
+        <div class="container-fluid">
+            @php $text =  Helper::text(252) @endphp
+            <div class="row quad-safari-slider">
+                @if ($text->image)<div class="col-md-3 col-6"><img src="{{Helper::image($text->image, 570,380, false)}}" alt="" class="img-fluid"></div>@endif
+                @for ($i = 2; $i <= 8; $i++)
+                @php
+                 $img = 'image'.$i;   
+                @endphp
+                    @if ($text->$img)
+                        <div class="col-md-3 col-6"><img src="{{Helper::image($text->$img, 570,380, false)}}" alt="slider" class="img-fluid"></div>
+                    @endif
+                @endfor
+            </div>
+        </div>
+    </section>
 </main>
 
 @endsection
